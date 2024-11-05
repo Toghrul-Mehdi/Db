406e4dc628e4ae654c8d81ef063bc378dd371fbe
xaqan05 <xaqan2005@gmail.com>
2024-11-05 19:11:00 +0400

    db

---
+++ DB/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Server=localhost;Database=Departmentss;Trusted_Connection=True;";
+
+        string[] Name = { "Name1", "Name2", "Name3" };
+        string[] Surname = { "SurName1", "SurName2", "SurName3" };
+        double[] Salary = { 100.00, 200.00, 300.00 }; 
+        string[] Email = { "togrul@gmail.com", "turan@gmail.com", "kenan@gmail.com" };
+        string[] DepartmentName = { "Oba", "Araz", "Bravo" };
+
+        string query = "INSERT INTO Employees (Name, Surname, Salary, Email, DepartmentName) VALUES (@Name, @Surname, @Salary, @Email, @DepartmentName)";
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                try
+                {
+                    connection.Open();
+
+                    for (int i = 0; i < Name.Length; i++)
+                    {
+                        command.Parameters.Clear();
+                        command.Parameters.AddWithValue("@Name", Name[i]);
+                        command.Parameters.AddWithValue("@Surname", Surname[i]);
+                        command.Parameters.AddWithValue("@Salary", Salary[i]);
+                        command.Parameters.AddWithValue("@Email", Email[i]);
+                        command.Parameters.AddWithValue("@DepartmentName", DepartmentName[i]);
+
+                        int rowsAffected = command.ExecuteNonQuery();
+                        Console.WriteLine($"{rowsAffected} row(s) added: {Name[i]}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
